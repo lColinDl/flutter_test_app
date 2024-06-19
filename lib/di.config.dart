eb91b073589e4d42855e73687ed0176eb511f34f
daumc <daumc@mediamarktsaturn.com>
2024-06-19 13:39:53 +0200

    Add new dependency

--- lib/di.config.dart
+++ lib/di.config.dart
@@ -8,8 +8,9 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter_test_app/bar.dart' as _i3;
-import 'package:flutter_test_app/foo.dart' as _i4;
+import 'package:flutter_test_app/bar.dart' as _i4;
+import 'package:flutter_test_app/foo.dart' as _i5;
+import 'package:flutter_test_app/new_dependency.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -24,8 +25,12 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.singleton<_i3.Bar>(() => _i3.Bar());
-    gh.singleton<_i4.Foo>(() => _i4.Foo(gh<_i3.Bar>()));
+    gh.singleton<_i3.NewDependency>(() => _i3.NewDependency());
+    gh.singleton<_i4.Bar>(() => _i4.Bar());
+    gh.singleton<_i5.Foo>(() => _i5.Foo(
+          gh<_i4.Bar>(),
+          gh<_i3.NewDependency>(),
+        ));
     return this;
   }
 }
